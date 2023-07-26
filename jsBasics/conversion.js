733fc4386f711c920ad3653daabfdcd473dee469
codeSparrow092 <140591560+codeSparrow092@users.noreply.github.com>
2023-07-26 09:43:17 +0000

    ok

---
+++ jsBasics/conversion.js
@@ -0,0 +1,23 @@
+let num = 893;
+let str = "hello"
+let boolean = true
+let unde
+
+let numToStr = String(num) //it convert number to string
+console.log(numToStr)
+console.log(typeof numToStr)
+
+let strNum = Number(str) //it convert string to number
+console.log(strNum)      //it gives o/p => NaN(Not a Number)
+console.log(typeof strNum)
+
+let numBol = Boolean(num) //it convert NUmber to Boolean
+console.log(numBol)       //o/p=>true
+console.log(typeof numBol)
+
+let bolNum = Number(boolean) //it convert Boolean to Number 
+console.log(bolNum)      //o/p=>1
+console.log(typeof bolNum)
+
+
+
--- jsBasics/dataTypes.js
+++ jsBasics/dataTypes.js
@@ -1,13 +1,12 @@
-let name = "ANil"
-let id = 845
-let key = null
-let age;
+let name = "ANil";
+let id = 845;
+let key = null; //object
+let age; //undefined
 
-console.log(typeof name)
-console.log(typeof id)
-console.log(typeof key)
-console.log(typeof age)
-
+console.log(typeof name);
+console.log(typeof id);
+console.log(typeof key);
+console.log(typeof age);
 
 /*
 Datatypes in js:
@@ -15,7 +14,7 @@ Datatypes in js:
 2.number => any number
 3.bigInt => big numbers
 4.null => null value
-5.undefind => variable not have any value
+5.undefined => variable not have any value
 6.boolean => ture/false
 7.symbol => unique
 8.Array => []
