f846cd274219a9b277f4e625bd88acc5773a22ea
codeSparrow092 <140591560+codeSparrow092@users.noreply.github.com>
2023-07-26 08:50:22 +0000

    datatypes

---
+++ jsBasics/dataTypes.js
@@ -0,0 +1,25 @@
+let name = "ANil"
+let id = 845
+let key = null
+let age;
+
+console.log(typeof name)
+console.log(typeof id)
+console.log(typeof key)
+console.log(typeof age)
+
+
+/*
+Datatypes in js:
+1.string => ""
+2.number => any number
+3.bigInt => big numbers
+4.null => null value
+5.undefind => variable not have any value
+6.boolean => ture/false
+7.symbol => unique
+8.Array => []
+9.object => :{}
+10.date =>
+
+*/
---
+++ jsBasics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 21324;
+var name = "ANIl"
+let address = "delhi india"
+let eMail = "example@google.com"
+place = "pune"
+
+//accountId = 943048; //this is not possible..
+
+/*
+Best keyword is use for script is const and let .
+do not use var keyword for variable decleration because of the scopes and blocks.
+thats all
+*/
+
+console.log([accountId, name, address, eMail, place])
+
+console.table([accountId,name,address,eMail,place])
